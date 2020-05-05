28f68e8d913808002d1f0bec8685becf93e9db2c
Breiden Busch <breiden.busch@gmail.com>
2020-05-05 16:18:25 -0600

    Initial commit

--- src/App.js
+++ src/App.js
@@ -1,26 +1,45 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from 'react'
+import './App.css'
+import { RedocStandalone } from 'redoc'
+import AnalyticsApi from './analytics.json'
+require('typeface-inter')
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <RedocStandalone
+        spec={AnalyticsApi}
+        options={{
+          theme: {
+            colors: {
+              primary: { main: '#165fcf' },
+              success: { main: '#208844' },
+              warning: { main: '#f7b148' },
+              error: { main: '#dc3552' },
+              text: {
+                primary: '#5b616c',
+              },
+              http: {
+                get: '#468fff',
+                post: '#5eac7d',
+                delete: '#e37383',
+                put: '#f7b148',
+              },
+            },
+            typography: {
+              fontFamily: 'Inter',
+              headings: {
+                fontFamily: 'Inter',
+              },
+            },
+            sidebar: {
+              textColor: '#5b616c',
+            },
+          },
+        }}
+      />
     </div>
-  );
+  )
 }
 
-export default App;
+export default App
